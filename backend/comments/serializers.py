d57840884e3dace03b5783f2f4d0d737a7894971
Chris Creamer <chrisc1983@gmail.com>
2022-05-03 18:47:03 -0700

    finished the first part of the first views function for the locations app and tested to success in Postman

---
+++ backend/comments/serializers.py
@@ -0,0 +1,8 @@
+from rest_framework import serializers
+from .models import Comment
+
+class CommentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Comment
+        fields = ['id', 'comment_text', 'user_id', 'event_id']
+        depth = 1
--- backend/locations/views.py
+++ backend/locations/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from .models import Location
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.response import Response
+from .serializers import LocationSerializer
+from django.shortcuts import get_object_or_404
+from rest_framework import status
+from rest_framework.permissions import AllowAny, IsAuthenticated
 
-# Create your views here.
+@api_view(['GET'])
+@permission_classes([AllowAny])
+def get_all_locations(request):
+    locations = Location.objects.all()
+    serializer = LocationSerializer(locations, many=True)
+    return Response(serializer.data, status=status.HTTP_200_OK)
+
+
